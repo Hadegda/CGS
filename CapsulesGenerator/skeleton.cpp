a7cb0b93ed129b3dc627249f5e39f5eab6d97727
Надежда Кацман <katsman96@yandex.ru>
2018-04-16 20:29:02 +0300

    rewrite updating caps for new parameters in skeleton

--- CapsulesGenerator/skeleton.cpp
+++ CapsulesGenerator/skeleton.cpp
@@ -143,23 +143,11 @@ Skeleton::Skeleton(PersonPattern* homo, DirectX::XMFLOAT3 color)
 	this->color = color;
 	homo->GetParameters(&parameters);
 
-	/*DirectX::XMFLOAT4* capsules;
-	capsules = new DirectX::XMFLOAT4[28];*/
-
 	caps.clear();
 	caps.resize(homo->GetCapsCount());
 	for (int i = 0; i < caps.size(); i++)
 		caps[i] = new Capsule(color);
 	UpdateForNewParameters();
-
-	/*caps.clear();
-	caps.resize(homo->GetCapsCount());
-	for (int i = 0; i < caps.size(); i++)
-		caps[i] = new Capsule(color);
-	for (int i = 0; i < 14; i++)
-		caps[i]->Init(capsules[2 * i], capsules[2 * i + 1]);
-
-	delete[] capsules;*/
 }
 
 Skeleton::~Skeleton()
@@ -197,57 +185,18 @@ void Skeleton::UpdateForNewParameters()
 	UpdateOneCapsule(&p1, p0, 3, false);
 	caps[0]->Init(p0, p1);
 	
-	//left leg
-	UpdateOneCapsule(&p0, caps[0]->p0, 8, true);
-	UpdateOneCapsule(&p1, p0, 12, false);
-	caps[1]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[1]->p1, 17, true);
-	UpdateOneCapsule(&p1, p0, 21, false);
-	caps[2]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[2]->p1, 26, true);
-	UpdateOneCapsule(&p1, p0, 30, false);
-	caps[3]->Init(p0, p1);
-
-	//right leg
-	UpdateOneCapsule(&p0, caps[0]->p0, 35, true);
-	UpdateOneCapsule(&p1, p0, 39, false);
-	caps[4]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[4]->p1, 44, true);
-	UpdateOneCapsule(&p1, p0, 48, false);
-	caps[5]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[5]->p1, 53, true);
-	UpdateOneCapsule(&p1, p0, 57, false);
-	caps[6]->Init(p0, p1);
-
-	//left Arm
-	UpdateOneCapsule(&p0, caps[0]->p1, 62, true);
-	UpdateOneCapsule(&p1, p0, 66, false);
-	caps[7]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[7]->p1, 71, true);
-	UpdateOneCapsule(&p1, p0, 75, false);
-	caps[8]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[8]->p1, 80, true);
-	UpdateOneCapsule(&p1, p0, 84, false);
-	caps[9]->Init(p0, p1);
-
-	//right Arm
-	UpdateOneCapsule(&p0, caps[0]->p1, 89, true);
-	UpdateOneCapsule(&p1, p0, 93, false);
-	caps[10]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[10]->p1, 98, true);
-	UpdateOneCapsule(&p1, p0, 102, false);
-	caps[11]->Init(p0, p1);
-
-	UpdateOneCapsule(&p0, caps[11]->p1, 107, true);
-	UpdateOneCapsule(&p1, p0, 111, false);
-	caps[12]->Init(p0, p1);
+	for (int i = 1, k = 8; i < 13; i++, k +=9 ) {
+		if(i == 1 || i == 4)  //begin legs
+			UpdateOneCapsule(&p0, caps[0]->p0, k, true);
+		else {
+			if (i == 7 || i == 10)  //begin arms
+				UpdateOneCapsule(&p0, caps[0]->p1, k, true);
+			else
+				UpdateOneCapsule(&p0, caps[i - 1]->p1, k, true);
+		}
+		UpdateOneCapsule(&p1, p0, k+4, false);
+		caps[i]->Init(p0, p1);
+	}
 
 	//head
 	UpdateOneCapsule(&p0, caps[0]->p1, 116, true);
